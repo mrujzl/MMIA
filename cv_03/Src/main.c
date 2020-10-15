5439faaedffc7559f79799baf78e52ccd3cf8294
Miroslav Rujzl <mirek.rujzl@seznam.cz>
2020-10-15 15:56:44 +0200

    cv_03_bod_2

--- cv_03/Src/main.c
+++ cv_03/Src/main.c
@@ -28,10 +28,15 @@ int main(void)
 {
 	sct_init();
 	sct_led(0x7a5c36de);
+	for(volatile uint32_t j = 0; j < 3000000; j++);
 
     /* Loop forever */
 	while(1)
 	{
-
+		for(uint16_t i = 0; i < 1000; i+= 111)
+		{
+			sct_value(i);
+			for(volatile uint32_t j = 0; j < 300000; j++);
+		}
 	}
 }
--- cv_03/Src/sct.c
+++ cv_03/Src/sct.c
@@ -3,36 +3,102 @@
  *
  *  Created on: 15. 10. 2020
  *      Author: Mirek
+ *
+ *      Driver for register SCT2024
  */
 
 #include "stm32f0xx.h"
 #include "sct.h"
 
-#define sct_nla(x) do { if (x) GPIOB->BSRR = (1 << 5); else GPIOB->BRR = (1 << 5); } while (0)
-#define sct_sdi(x) do { if (x) GPIOB->BSRR = (1 << 4); else GPIOB->BRR = (1 << 4); } while (0)
-#define sct_clk(x) do { if (x) GPIOB->BSRR = (1 << 3); else GPIOB->BRR = (1 << 3); } while (0)
-#define sct_noe(x) do { if (x) GPIOB->BSRR = (1 << 10); else GPIOB->BRR = (1 << 10); } while (0)
+#define sct_nla(x) do { if (x) GPIOB->BSRR = (1 << 5); else GPIOB->BRR = (1 << 5); } while (0) 		// Latch pin ON/OFF
+#define sct_sdi(x) do { if (x) GPIOB->BSRR = (1 << 4); else GPIOB->BRR = (1 << 4); } while (0)		// Data pin ON/OFF
+#define sct_clk(x) do { if (x) GPIOB->BSRR = (1 << 3); else GPIOB->BRR = (1 << 3); } while (0)		// Clock pin ON/OFF
+#define sct_noe(x) do { if (x) GPIOB->BSRR = (1 << 10); else GPIOB->BRR = (1 << 10); } while (0)	// Enable pin ON/OFF
 
-
+/*
+ * sct_led
+ * 	- Set the input variable "value" to SCT
+ */
 void sct_led(uint32_t value)
 {
 	for(uint8_t i = 0; i < 32; i++)
 	{
-		sct_sdi(value & 1);
+		sct_sdi(value & 1); // Set the LSB
 		value >>= 1;
-		sct_clk(1);
+		sct_clk(1);		// Time tick
 		sct_clk(0);
 	}
 
-	sct_nla(1);
+	sct_nla(1);			// Latch tick
 	sct_nla(0);
 }
 
-
+/*
+ * sct_init
+ * 	- Initialization for SCT
+ */
 void sct_init(void)
 {
 	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;		// Set clock for GPIOB
 	GPIOB->MODER |= GPIO_MODER_MODER5_0 | GPIO_MODER_MODER4_0 | GPIO_MODER_MODER3_0 | GPIO_MODER_MODER10_0;	// GPIO port mode: PB5, PB4, PB3, PB10 output
-	sct_noe(0);
+	sct_noe(0);		// Default values
 	sct_led(0);
+}
+
+
+/*
+ * sct_value
+ * 	- Lookup table for 3 digits
+ */
+void sct_value(uint16_t value)
+{
+	static const uint32_t reg_values[3][10] = {
+	{
+		//PCDE--------GFAB @ DIS1
+		0b0111000000000111 << 16,
+		0b0100000000000001 << 16,
+		0b0011000000001011 << 16,
+		0b0110000000001011 << 16,
+		0b0100000000001101 << 16,
+		0b0110000000001110 << 16,
+		0b0111000000001110 << 16,
+		0b0100000000000011 << 16,
+		0b0111000000001111 << 16,
+		0b0110000000001111 << 16,
+	},
+	{
+		//----PCDEGFAB---- @ DIS2
+		0b0000011101110000 << 0,
+		0b0000010000010000 << 0,
+		0b0000001110110000 << 0,
+		0b0000011010110000 << 0,
+		0b0000010011010000 << 0,
+		0b0000011011100000 << 0,
+		0b0000011111100000 << 0,
+		0b0000010000110000 << 0,
+	 	0b0000011111110000 << 0,
+	 	0b0000011011110000 << 0,
+	},
+	{
+		//PCDE--------GFAB @ DIS3
+		0b0111000000000111 << 0,
+		0b0100000000000001 << 0,
+		0b0011000000001011 << 0,
+		0b0110000000001011 << 0,
+		0b0100000000001101 << 0,
+		0b0110000000001110 << 0,
+	 	0b0111000000001110 << 0,
+		0b0100000000000011 << 0,
+		0b0111000000001111 << 0,
+		0b0110000000001111 << 0,
+	},
+	};
+
+	uint32_t reg = 0;
+
+	reg |= reg_values[0][value / 100 % 10]; 	// Hundred
+	reg |= reg_values[1][value / 10 % 10];		// Hecade
+	reg |= reg_values[2][value / 1 % 10];		// Ones
+
+	sct_led(reg);	// Set value to SCT
 }
